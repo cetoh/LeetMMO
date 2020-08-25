300966322b59942c0213bd2246c5966b28cd6e65
Chris Toh <31299566+cetoh@users.noreply.github.com>
2020-08-25 14:56:15 -0700

    Add self public reference of plugin

--- LeetMMO/src/main/java/us/toh/leetmmo/LeetMMO.java
+++ LeetMMO/src/main/java/us/toh/leetmmo/LeetMMO.java
@@ -4,10 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LeetMMO extends JavaPlugin {
 
+    public static LeetMMO plugin;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        plugin = this;
+        String path = this.getDataFolder().getAbsolutePath();
         System.out.println("LeetMMO Enabled");
+
     }
 
     @Override
