24a0942a65de4b2a35db2fadfb7c5236065157fb
AndrewEToh <Zelkanok@gmail.com>
2020-08-25 16:38:23 -0700

    Updated PlayerProfile

--- LeetMMO/src/main/java/us/toh/leetmmo/datatypes/player/PlayerProfile.java
+++ LeetMMO/src/main/java/us/toh/leetmmo/datatypes/player/PlayerProfile.java
@@ -1,7 +1,10 @@
 package us.toh.leetmmo.datatypes.player;
 
 import org.bukkit.entity.Player;
-import us.toh.leetmmo.datatypes.*;
+import us.toh.leetmmo.datatypes.experience.*;
+import us.toh.leetmmo.datatypes.level.*;
+import us.toh.leetmmo.datatypes.skillpoint.*;
+
 
 import java.util.UUID;
 
@@ -14,11 +17,23 @@ public class PlayerProfile {
     private ClassExperiencePool cEXPPool;
     private NormalSkillPointPool nSPPool;
     private ClassSkillPointPool cSPPool;
+    private NormalLevel nLvl;
+    private ClassLevel cLvl;
+
+    public enum expType {NORMAL, CLASS};
+
 
     public PlayerProfile(Player player) {
         this.player = player;
         this.playerName = player.getName();
         this.uuid = player.getUniqueId();
+
+        nEXPPool = new NormalExperiencePool(0, 100);
+        cEXPPool = new ClassExperiencePool(0, 100);
+        nSPPool = new NormalSkillPointPool(0);
+        cSPPool = new ClassSkillPointPool(0);
+        nLvl = new NormalLevel();
+        cLvl = new ClassLevel();
     }
 
     public String getPlayerName() {
@@ -32,6 +47,33 @@ public class PlayerProfile {
     public void setUuid(UUID uuid) {
         this.uuid = uuid;
     }
+
+    public void addExperience (double exp, PlayerProfile.expType t ) {
+        switch(t) {
+            case NORMAL:
+                nEXPPool.addPoints(exp);
+                if (nEXPPool.getCarry() > 0) {
+                    nLvl.increaseLevel();
+                    nEXPPool.setPoolCap(nLvl.getCurrentLevel()*100);
+                    nSPPool.addNumPoints(1);
+                    nEXPPool.setPoints(nEXPPool.getCarry());
+                    nEXPPool.setCarry(0);
+                }
+                break;
+            case CLASS:
+                cEXPPool.addPoints(exp);
+                if (cEXPPool.getCarry() > 0) {
+                    cLvl.increaseLevel();
+                    cEXPPool.setPoolCap(cLvl.getCurrentLevel()*100);
+                    cSPPool.addNumPoints(1);
+                    cEXPPool.setPoints(cEXPPool.getCarry());
+                    cEXPPool.setCarry(0);
+                }
+                break;
+        }
+    }
+
+
 
 
 }
