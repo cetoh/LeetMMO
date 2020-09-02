09ee34fca3512017d8fb936a8dddc92173c664dd
Chris Toh <31299566+cetoh@users.noreply.github.com>
2020-09-02 15:36:43 -0700

    Added add point command for farming

--- LeetMMO/src/main/java/us/toh/leetmmo/commands/CommandLeetFarming.java
+++ LeetMMO/src/main/java/us/toh/leetmmo/commands/CommandLeetFarming.java
@@ -6,10 +6,13 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 import us.toh.leetmmo.datatypes.player.PlayerProfile;
 import us.toh.leetmmo.skills.Skill;
 import us.toh.leetmmo.skills.normal.NormalSkillEnums;
 import us.toh.leetmmo.skills.normal.farming.skilltree.FarmingSkillTree;
+import us.toh.leetmmo.utils.EnumUtilities;
+import us.toh.leetmmo.utils.StringUtilities;
 
 import java.util.HashMap;
 import java.util.List;
@@ -20,11 +23,12 @@ import static us.toh.leetmmo.skills.normal.NormalSkillEnums.FarmingSkillNames.*;
 
 public class CommandLeetFarming implements CommandExecutor {
 
+    private Player player;
     private Map<UUID, PlayerProfile> globalPlayers;
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player player = (Player) sender;
+        player = (Player) sender;
 
         PlayerProfile profile = globalPlayers.get(player.getUniqueId());
 
@@ -32,14 +36,13 @@ public class CommandLeetFarming implements CommandExecutor {
         if (args.length == 0) {
             player.sendMessage("Available Normal Skill Points: " + ChatColor.GOLD + profile.getnSPPool().getNumPoints() + ChatColor.GOLD + " pts");
 
-            displayWholeTree(player, profile);
+            displayWholeTree(profile);
         } else {
             //Check if first argument is a valid Farming skill. If it is display the skill info if no other command follows.
             if (!args[0].isEmpty() && args[0] != null && !args[0].equals("")
                 && (args[1].isEmpty() || args[1] == null)) {
-                if (NormalSkillEnums.isInEnum(args[0], NormalSkillEnums.FarmingSkillNames.class)) {
-                    sendSkillMessage(player,
-                            profile.getFarmingSkillTree()
+                if (EnumUtilities.isInEnum(args[0], NormalSkillEnums.FarmingSkillNames.class)) {
+                    sendSkillMessage(profile.getFarmingSkillTree()
                                     .getTree()
                                     .get(NormalSkillEnums.FarmingSkillNames.valueOf(args[0])));
                 } else {
@@ -49,14 +52,67 @@ public class CommandLeetFarming implements CommandExecutor {
 
             //Check if add skill point command was initiated
             else if (!args[0].isEmpty() && args[0] != null && !args[0].equals("")
-                    && !args[1].isEmpty() && args[1] != null) {
+                    && !args[1].isEmpty() && args[1] != null
+                    && !args[2].isEmpty() && args[2] != null) {
+
+                if (args[1].equals("add")) {
+                    if (!EnumUtilities.isInEnum(args[0], NormalSkillEnums.FarmingSkillNames.class)) {
+                        player.sendMessage(ChatColor.RED + "Unrecognized Skill");
+                    } else {
+                        if (StringUtilities.isInteger(args[2])) {
+                            //Add point to skill
+                            addSkillPointToSkill(profile,
+                                    NormalSkillEnums.FarmingSkillNames.valueOf(args[0]),
+                                    Integer.getInteger(args[2]));
+                        }
+                        else {
+                            player.sendMessage(ChatColor.RED + "Must be a valid integer. (e.g. leet[skilltreename] [skillName] add INTEGER)");
+                        }
+                    }
+
+                } else {
+                    player.sendMessage(ChatColor.RED + "Invalid Command");
+                }
             }
         }
         return true;
     }
 
+    private void addSkillPointToSkill(PlayerProfile playerProfile, Enum skillEnum, int ptsToAdd) {
 
-    private void sendSkillMessage(Player player, Skill skill) {
+        //Check that player has skill points available
+        if (playerProfile.getnSPPool().getNumPoints() >= ptsToAdd) {
+            HashMap<Enum, Skill> tree = playerProfile.getFarmingSkillTree().getTree();
+
+            Skill skill = tree.get(skillEnum);
+
+            //Add skill points but only to cap
+            int remainder = 0;
+            for (int i = 1; i < ptsToAdd + 1; i++) {
+                if (skill.getSkillPoints() < skill.getSkillPointRequirement()) {
+                    skill.addSkillPoint();
+                } else {
+                    remainder = ptsToAdd - i;
+                }
+            }
+
+            //Update tree
+            tree.put(skillEnum, skill);
+
+            //Remove used skill points from pool
+            playerProfile.getnSPPool().subtractNumPoints(ptsToAdd - remainder);
+
+            //Notify user of action
+            String notif = (ptsToAdd - remainder) + " points were added to " + skillEnum.toString() + ". "
+                    + remainder + " points could not be added.";
+            
+            player.sendMessage(ChatColor.YELLOW + notif);
+        } else {
+            player.sendMessage(ChatColor.RED + "Insufficient Normal Skill Points!");
+        }
+    }
+
+    private void sendSkillMessage(Skill skill) {
         Enum name = skill.getSkillName();
         String description = skill.getDescription();
         int numPoints = skill.getSkillPoints();
@@ -93,7 +149,7 @@ public class CommandLeetFarming implements CommandExecutor {
         player.sendMessage(msg);
     }
 
-    private void displayWholeTree(Player player, PlayerProfile playerProfile) {
+    private void displayWholeTree(PlayerProfile playerProfile) {
         HashMap<Enum, Skill> skill = playerProfile.getFarmingSkillTree().getTree();
 
         String msg = "";
--- LeetMMO/src/main/java/us/toh/leetmmo/skills/normal/NormalSkillEnums.java
+++ LeetMMO/src/main/java/us/toh/leetmmo/skills/normal/NormalSkillEnums.java
@@ -2,21 +2,6 @@ package us.toh.leetmmo.skills.normal;
 
 public class NormalSkillEnums {
 
-    /**
-     * This is a utility method to check if a given string is in specific enum usage is as such:
-     * .isInEnum(string, Enum.class)
-     * @param value
-     * @param enumClass
-     * @param <E>
-     * @return
-     */
-    public static <E extends Enum<E>> boolean isInEnum(String value, Class<E> enumClass) {
-        for (E e : enumClass.getEnumConstants()) {
-            if(e.name().equals(value)) { return true; }
-        }
-        return false;
-    }
-
     public enum ConstructionSkillNames {
 
     }
